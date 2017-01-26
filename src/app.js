ae46672a6f88f6768b9c1e199bf6c00bae88d909
Andreas Müller <mail@devmount.de>
2017-01-26 13:57:13 +0100

    added: test chart

--- src/app.js
+++ src/app.js
@@ -118,6 +118,35 @@ var Dashboard = Vue.extend({
         return {
             admin: admin
         };
+    },
+    mounted: function() {
+        // And for a doughnut for song languages
+        var languages = new Chart('languages', {
+            type: 'doughnut',
+            data: {
+                labels: [
+                    "Red",
+                    "Blue",
+                    "Yellow"
+                ],
+                datasets: [{
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+            },
+            options: {
+                responsive: true
+            }
+        });
     }
 });
 
