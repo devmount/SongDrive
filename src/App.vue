21e14697037afb3400fea912ed332b9e3f4a2b59
devmount <mail@devmount.de>
2018-11-11 14:27:43 +0100

    :repeat: init v1 with router, pwa and sass

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,14 +18,12 @@
 }
 #nav {
   padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
