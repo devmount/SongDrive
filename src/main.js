f1d35e6860bdeda00de2c112d831237e3b2f9d08
devmount <mail@devmount.de>
2018-11-15 00:39:13 +0100

    :repeat: install firebase, vuefire

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+import VueFire from 'vuefire'
+Vue.use(VueFire)
+
 Vue.config.productionTip = false
 
 new Vue({
