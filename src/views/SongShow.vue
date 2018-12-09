c03ec4615ee3e2fb6212701327eb402a5dabbf9e
Andreas Müller <mail@devmount.de>
2018-12-09 23:38:55 +0100

    :heavy_plus_sign: add pdf export

--- src/views/SongShow.vue
+++ src/views/SongShow.vue
@@ -63,7 +63,7 @@
         <button class="btn btn-secondary d-block stretch mb-1" @click="exportSng">
           <i class="icon icon-download"></i><span class="hide-lg text-pre"> .SNG</span>
         </button>
-        <button class="btn btn-secondary d-block stretch disabled">
+        <button class="btn btn-secondary d-block stretch" @click="exportPdf">
           <i class="icon icon-download"></i><span class="hide-lg text-pre"> .PDF</span>
         </button>
       </div>
@@ -100,6 +100,18 @@
 import SongContent from '@/components/SongContent.vue'
 // get database object authorized in config.js
 import { db } from '../firebase'
+// pdf creation
+var pdfMake = require("pdfmake/build/pdfmake.js")
+var pdfFonts = require('@/assets/vfs_fonts.js')
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+pdfMake.fonts = {
+  FiraMono: {
+    normal: 'FiraMono-Regular.otf',
+  },
+  Roboto: {
+    normal: 'Roboto-Regular.ttf',
+  }
+}
 
 export default {
   name: 'song-show',
@@ -196,6 +208,218 @@ export default {
         .replace(/--o/g, "outro")
       // start download
       this.download(content, this.song['.key'] + '.sng')
+    },
+    exportPdf: function() {
+      var content = this.getPdfSongContent()
+      // return page configuration with computed content
+      var doc = {
+        pageSize: 'A4',
+        pageMargins: [ 50, 50, 40, 30 ],
+        content: content,
+        styles: {
+          header: {
+            font: 'Roboto',
+            fontSize: 22
+          },
+          partnumber: {
+            font: 'Roboto',
+            fontSize: 24,
+            margin: [ 0, 17, 0, 0 ]
+          },
+          code: {
+            font: 'FiraMono',
+            fontSize: 10.5,
+            margin: [ 0, 15, 0, 0 ]
+          },
+          copyright: {
+            font: 'Roboto',
+            fontSize: 8,
+            margin: [ 0, 20, 0, 0 ]
+          }
+        }
+      }
+      pdfMake.createPdf(doc).open()
+    },
+    getPdfSongContent () {
+      // handle song content parts
+      var content = [], parts = this.parsedContent(this.song.content)
+      parts.forEach(function(part) {
+        if (part.type == 'v' && part.number != '0') {
+          content.push({
+          columnGap: 8,
+          columns: [
+          {
+            style: 'partnumber',
+            width: 'auto',
+            text: part.number
+          },
+          {
+                style: 'code',
+            width: '*',
+                // song content with respect to leading whitespaces
+            text: '\u200B' + part.content.replace(/\n/g, "\n" + '\u200B')
+          }
+          ]
+          })
+        } else {
+          content.push({
+            style: 'code',
+            // song content with respect to leading whitespaces
+            text: '\u200B' + part.content.replace(/\n/g, "\n" + '\u200B')
+          })
+        }
+      }, this)
+      // return array with song data
+      return [
+        // song title [tuning] with a line beneath
+        { text: this.song.title.toUpperCase() + (this.song.tuning ? '  [' + this.song.tuning + ']' : ''), style: 'header' },
+        { canvas: [{ type: 'line', x1: 0, y1: 0, x2: 505, y2: 0, lineWidth: .5 }] },
+        content,
+        // imprint with ccli#, author names and (c) year publisher
+        {
+          style: 'copyright',
+          text: [
+            this.song.ccli ? 'CCLI Song Nr.: ' + this.song.ccli + '\n' : '',
+            this.song.authors ? this.song.authors.join(', ') + '\n' : '',
+            '\u00A9 ' + this.song.year + ' ' + this.song.publisher
+          ]
+        }
+      ]
+    },
+    parsedContent () {
+      // initialize arrays for parsed linex, classes of parts, type abbr., numbers of type and part index
+      var parsed = [], classes = [], types = [], numbers = [], part = 0
+      var lines = this.song.content.split('\n')
+      // check every single line of song content
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i]
+        // handle display chord lines
+        if (this.isChordLine(line) && !this.chords) {
+          // skip chord line if no chords shall be displayed
+          continue
+        }
+        // handle chord tuning
+        if (this.isChordLine(line) && this.tuning != 0) {
+          // init the new line to build and the current over- or underflow of spaces due to different chord string lenghts
+          var newLine = '', spaces = 0, j = 0
+          while (j < line.length) {
+            // get single character in line
+            var c = line[j]
+            // handle over- or underflow of spaces to keep chords on their original position
+            if (spaces > 0 && c == ' ') {
+              // to few spaces: double next existing space and decrease space count
+              c = '  '
+              spaces--
+            }
+            if (spaces < 0 && c == ' ' && line[j+1] == ' ') {
+              // to many spaces: skip one of two consecutive spaces and increase space count
+              spaces++
+              j++
+              continue
+            }
+            // on '#': skip to next character as it will be handled together with tune
+            if (c == '#') {
+              j++
+              continue
+            }
+            var isHalf = line[j+1] == '#'
+            // check if character is a tune with '#'
+            if (isHalf) {
+              c = c + '#'
+            }
+            // check if character is a transposable character
+            if (this.tunes.indexOf(c) > -1) {
+              // replace character by next tune character
+              var nextTune = this.tunes[(12 + this.tunes.indexOf(c) + (this.tuning % 12)) % 12]
+              newLine += nextTune
+              // update over- or underflow of spaces
+              spaces += c.length - nextTune.length
+            } else {
+              newLine += c
+            }
+            j++
+          }
+          // make sure that last two characters stay spaces for chord line identification (rtrim and add 2 spaces)
+          newLine = newLine.replace(/\s+$/, '') + '  '
+          // add lines to new content
+          if (!parsed[part]) {
+            parsed[part] = []
+          }
+          parsed[part].push(newLine)
+          continue
+        }
+        // handle normal song line
+        if (line.trim().indexOf('--') < 0) {
+          // only consider line if not empty
+          if (line.trim() != '') {
+            if (!parsed[part]) {
+              parsed[part] = []
+            }
+            // add line to current part
+            parsed[part].push(line)
+          }
+        }
+        // handle song part marker (e.g. --V1)
+        else {
+          // add class to part
+          switch (line.charAt(2).toLowerCase()) {
+            case 'v':
+              types.push('v')
+              classes.push('verse ' + ((!isNaN(parseInt(line.trim().charAt(3)))) ? 'part' + line.trim().charAt(3) : ''))
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'p':
+              types.push('p')
+              classes.push('prechorus')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'c':
+              types.push('c')
+              classes.push('chorus')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'b':
+              types.push('b')
+              classes.push('bridge')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'i':
+              types.push('i')
+              classes.push('intro')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'm':
+              types.push('m')
+              classes.push('mitro')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            case 'o':
+              types.push('o')
+              classes.push('outro')
+              numbers.push((!isNaN(parseInt(line.trim().charAt(3)))) ? line.trim().charAt(3) : '0'); break
+            default:
+              // a non existent part tag was found
+          }
+          // consider next part
+          part++
+        }
+      }
+      var newContent = []
+      // if multiple parts: rejoin lines of every part
+      if (parsed.length > 1) {
+        for (var p = 1; p < parsed.length; p++) {
+          newContent.push({
+            type: types[p-1],
+            number: numbers[p-1],
+            class: classes[p-1],
+            content: parsed[p].join('\n')
+          })
+        }
+      }
+      // if no parts (no markers set): take whole content as one unclassified part
+      else {
+        newContent.push({
+          type: '',
+          number: 0,
+          class: '',
+          content: this.content
+        })
+      }
+      return newContent
     },
     download (data, filename) {
       var a = document.createElement('a')
