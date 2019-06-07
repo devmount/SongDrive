73fe309509817af952c8e6c3facba0a0db6cef08
Andreas Müller <mail@devmount.de>
2019-06-08 01:08:44 +0200

    :gear: start dashboard implementation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,14 +8,46 @@
 						Dashboard
 					</h2>
 				</div>
+				<div class="column col-4">
+					<h3>Latest Songs</h3>
+				</div>
+				<div class="column col-4">
+					<h3>Latest Setlists</h3>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+// get database object authorized in config.js
+import { db } from '@/firebase'
+
 export default {
 	name: 'dashboard',
+	firestore () {
+		return {
+			songs: {
+				ref: db.collection('songs'),
+				resolve: () => { this.ready.songs = true },
+				reject: () => { this.ready.songs = true }
+			},
+			setlists: {
+				ref: db.collection('setlists').orderBy('date', 'desc'),
+				resolve: () => { this.ready.setlists = true },
+				reject: () => { this.ready.setlists = true }
+			},
+			tags: db.collection('tags'),
+		}
+	},
+	data () {
+		return {
+			ready: {
+				songs: false,
+				setlists: false,
+			},
+		}
+	},
 }
 </script>
 
