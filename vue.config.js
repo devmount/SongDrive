1d76e2231d82252bb75e94fed7dccbf5d4bc4ecb
Andreas Müller <mail@devmount.de>
2020-02-14 08:55:14 +0100

    ✅ v1.1.2

--- vue.config.js
+++ vue.config.js
@@ -4,15 +4,15 @@ const packageJson = fs.readFileSync('./package.json')
 const version = JSON.parse(packageJson).version || 0
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
+	configureWebpack: {
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': {
 					PACKAGE_VERSION: '"' + version + '"'
 				}
-      })
-    ]
-  },
+			})
+		]
+	},
 	publicPath: '/',
 	chainWebpack: config => config.plugins.delete('named-chunks'),
 	parallel: false
