490ecd344990ada6da7d8d999e776ac1647eb142
Andreas Müller <mail@devmount.de>
2021-09-08 08:09:41 +0200

    💚 reindent and set semicolons on partials

--- src/partials/SongContent.vue
+++ src/partials/SongContent.vue
@@ -6,7 +6,11 @@
 			v-for="(parts, i) in parsedContent(content, tuning, chords, true)"
 			:key="i"
 		>
-			<pre v-for="(part, j) in parts" :key="j" :class="part.class"><span v-for="(line, l) in part.content.split('\n')" :key="l" :class="{ chords: isChordLine(line) }">{{ line }}</span></pre>
+			<pre v-for="(part, j) in parts" :key="j" :class="part.class"><!--
+				--><span v-for="(line, l) in part.content.split('\n')" :key="l" :class="{ chords: isChordLine(line) }"><!--
+					-->{{ line }}<!--
+				--></span><!--
+			--></pre>
 		</div>
 	</div>
 </template>
@@ -23,7 +27,7 @@ export default {
 	data () {
 		return {
 			margin: 40
-		}
+		};
 	},
 	methods: {
 		maximizeFontsize() {
@@ -31,45 +35,45 @@ export default {
 			for (let a of document.querySelectorAll('.present')) {
 				// all child elements
 				for (let b of a.querySelectorAll('pre')) {
-					let fontSize = parseInt(getComputedStyle(b).fontSize.match(/\d+/)[0])
+					let fontSize = parseInt(getComputedStyle(b).fontSize.match(/\d+/)[0]);
 					// increase font size as long as the child is still smaller than the parent with a max of 100 iterations
-					let n1 = 100
+					let n1 = 100;
 					while (b.offsetWidth < a.offsetWidth*.9 && n1 > 0) {
-						fontSize += 4
-						b.style.fontSize = fontSize + 'px'
-						n1--
+						fontSize += 4;
+						b.style.fontSize = fontSize + 'px';
+						n1--;
 					}
 					// decrease font size if the child width exceeds the parents width with a max of 100 iterations
-					let n2 = 100
+					let n2 = 100;
 					while (b.offsetWidth > a.offsetWidth*.9 && n2 > 0) {
-						b.style.fontSize = (fontSize--) + 'px'
-						n2--
+						b.style.fontSize = (fontSize--) + 'px';
+						n2--;
 					}
 				}
 			}
 			// decrease font size of parts with greatest font size first if it doesnt fit into viewport height
-			let vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)-85
+			let vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)-85;
 			// handle both columns
 			for (let c of document.querySelectorAll('.present')) {
-				let parts = []
+				let parts = [];
 				for (let d of c.querySelectorAll('pre')) {
 					parts.push({
 						part: d,
 						size: parseInt(getComputedStyle(d).fontSize.match(/\d+/)[0]),
 						height: d.offsetHeight+this.margin
-					})
+					});
 				}
 				// decrease font size of parts in columns with a greater height than viewport
 				// as long as the sum of the heights of the parts is greater than the viewport height with a max of 50 iterations
-				let n = 50
+				let n = 50;
 				while (parts.map(o=>o.height).reduce((p,c)=>p+c) > vh && n > 0) {
-					parts.sort(function(a, b) { return b.size - a.size; })
+					parts.sort(function(a, b) { return b.size - a.size; });
 					if (parts.length > 0) {
-						parts[0].part.style.fontSize = (parts[0].size - 5) + 'px'
-						parts[0].size = parseInt(getComputedStyle(parts[0].part).fontSize.match(/\d+/)[0])
-						parts[0].height = parts[0].part.offsetHeight+this.margin
+						parts[0].part.style.fontSize = (parts[0].size - 5) + 'px';
+						parts[0].size = parseInt(getComputedStyle(parts[0].part).fontSize.match(/\d+/)[0]);
+						parts[0].height = parts[0].part.offsetHeight+this.margin;
 					}
-					n--
+					n--;
 				}
 			}
 		}
