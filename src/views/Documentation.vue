f65c1c85fca20941baea5e6be071ca05407acf77
Andreas Müller <mail@devmount.de>
2021-10-23 20:59:46 +0200

    ➕ implement internal documentation page

---
+++ src/views/Documentation.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="shortcuts">
+		<div class="container no-sidebar">
+			<div class="columns">
+				<!-- heading -->
+				<div class="column col-12">
+					<h2 class="view-title">
+						{{ $t('page.docu') }}
+					</h2>
+				</div>
+				<div class="column col-4 col-xl-6 col-sm-12 mt-4">
+					tetst
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'documentation',
+}
+</script>
