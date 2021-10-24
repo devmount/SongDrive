a4251207922c2096b314a5273af1af3274709ef9
Andreas Müller <mail@devmount.de>
2021-10-24 07:05:49 +0200

    ➕ directly load md files of docs

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="shortcuts">
 		<div class="container no-sidebar">
+			<!-- title -->
 			<div class="columns text-center">
 				<!-- heading -->
 				<div class="column col-12 mb-15">
@@ -8,6 +9,7 @@
 					<h2 class="text-primary weight-normal mb-3">{{ $t('app.name') }}</h2>
 					<div class="text-gray">{{ $t('app.summary') }}</div>
 				</div>
+				<!-- features -->
 				<div class="columns col-8 col-xl-12 col-mx-auto">
 					<div v-for="i in 3" class="column col-4 col-xl-6 col-sm-12">
 						<ion-icon :name="$t('docu.features')[i-1].icon" class="icon-3x icon-thin mb-3"></ion-icon>
@@ -16,13 +18,26 @@
 					</div>
 				</div>
 			</div>
+			<!-- content -->
+			<div v-html="content"></div>
 		</div>
 	</div>
 </template>
 
 <script>
+const marked = require("marked");
+const docs = {
+	de: require("@/docs/docs.de.md"),
+	en: require("@/docs/docs.en.md"),
+};
+
 export default {
 	name: 'documentation',
+	computed: {
+		content () {
+			return marked(docs[this.$i18n.locale].default);
+		}
+	}
 }
 </script>
 
