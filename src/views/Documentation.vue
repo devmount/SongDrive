53d16f762a0058779b52cd999fa119ffeb5d1c6c
Andreas Müller <mail@devmount.de>
2021-10-24 06:25:55 +0200

    ➕ create documentation header

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class="shortcuts">
 		<div class="container no-sidebar">
-			<div class="columns">
+			<div class="columns text-center">
 				<!-- heading -->
-				<div class="column col-12">
-					<h2 class="view-title">
-						{{ $t('page.docu') }}
-					</h2>
+				<div class="column col-12 mb-15">
+					<img class="logo" src="@/assets/logo.svg" alt="SongDrive Song Management Tool" />
+					<h2 class="text-primary weight-normal mb-3">{{ $t('app.name') }}</h2>
+					<div class="text-gray">{{ $t('app.summary') }}</div>
 				</div>
-				<div class="column col-4 col-xl-6 col-sm-12 mt-4">
-					tetst
+				<div class="columns col-8 col-xl-12 col-mx-auto">
+					<div v-for="i in 3" class="column col-4 col-xl-6 col-sm-12">
+						<ion-icon :name="$t('docu.features')[i-1].icon" class="icon-3x icon-thin mb-3"></ion-icon>
+						<h3>{{ $t('docu.features')[i-1].title }}</h3>
+						<div>{{ $t('docu.features')[i-1].description }}</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -21,3 +25,10 @@ export default {
 	name: 'documentation',
 }
 </script>
+
+<style lang="scss" scoped>
+.logo {
+	width: 150px;
+	margin-top: 2rem;
+}
+</style>
