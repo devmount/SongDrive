b7af6d8bff3cc52c2d4c2b6705ec8951c947a62e
Andreas Müller <mail@devmount.de>
2021-11-28 20:53:43 +0100

    ❌ remove deprecated comment

--- src/partials/UserUnconfirmed.vue
+++ src/partials/UserUnconfirmed.vue
@@ -9,7 +9,6 @@
 		<div class="message">
 			<h3>{{ $t('text.waitingForApproval') }}</h3>
 			<p>{{ $t('text.notApprovedYet') }}</p>
-			<!-- TODO: load config.contact.email for unconfirmed users -->
 			<p v-if="ready.config && config.contact.email" v-html="$t('text.unconfirmedMistake', [config.contact.email])"></p>
 			<button class="btn btn-secondary d-block stretch mt-4" @click="$emit('signOut')">
 				{{ $t('button.signOut') }} <ion-icon name="log-out-outline" class="icon-right"></ion-icon>
