2566491b8adabbfea1c861fd6128d595ee6ff5d8
Andreas Müller <mail@devmount.de>
2021-12-30 22:43:27 +0100

    💚 rearrange login screen, add link for password reset

--- src/partials/Login.vue
+++ src/partials/Login.vue
@@ -6,30 +6,41 @@
 			<h2 class="text-primary weight-normal mt-4">{{ $t('app.name') }}</h2>
 		</div>
 		<!-- login -->
-		<div class="form-group">
-			<input
-				type="text"
-				v-model="email"
-				class="form-input mb-1"
-				:placeholder="$t('field.email')"
-				@keydown.enter="$emit('signIn', email, password)"
-			/>
-			<input
-				type="password"
-				v-model="password"
-				class="form-input mb-2"
-				:placeholder="$t('field.password')"
-				@keydown.enter="$emit('signIn', email, password)"
-			/>
-			<button class="btn btn-primary d-block stretch mb-2" @click="$emit('signIn', email, password)">
-				{{ $t('button.signIn') }} <ion-icon name="log-in-outline" class="icon-right"></ion-icon>
-			</button>
-			<button
-				class="btn d-block stretch btn-secondary"
-				@click="$emit('signUp')"
-			>
-				{{ $t('button.signUp') }} <ion-icon name="person-add-outline" class="icon-right"></ion-icon>
-			</button>
+		<div>
+			<div class="panel mb-5">
+				<div class="panel-header">
+					<div class="panel-title text-center">{{ $t('text.signInToSongDrive') }}</div>
+				</div>
+				<div class="panel-body">
+					<input
+						type="email"
+						v-model="email"
+						class="form-input mb-1"
+						:placeholder="$t('field.email')"
+						@keydown.enter="$emit('signIn', email, password)"
+					/>
+					<input
+						type="password"
+						v-model="password"
+						class="form-input"
+						:placeholder="$t('field.password')"
+						@keydown.enter="$emit('signIn', email, password)"
+					/>
+				</div>
+				<div class="panel-footer mt-3">
+					<button class="btn btn-primary btn-block" @click="$emit('signIn', email, password)">
+						{{ $t('button.signIn') }} <ion-icon name="log-in-outline" class="icon-right"></ion-icon>
+					</button>
+				</div>
+			</div>
+			<div class="px-4">
+				{{ $t('text.newToSongDrive') }}
+				<button class="btn btn-link" @click="$emit('signUp')">{{ $t('text.createAnAccount') }}</button>.
+			</div>
+			<div class="px-4">
+				{{ $t('text.forgotPassword') }}
+				<button class="btn btn-link" @click="$emit('resetPassword')">{{ $t('text.resetIt') }}</button>.
+			</div>
 		</div>
 	</div>
 </template>
@@ -59,6 +70,20 @@ export default {
 		margin: 0 auto;
 		max-width: 150px;
 		width: 100%;
+	}
+
+	.panel {
+		height: auto;
+
+	}
+
+	.btn-link {
+		margin: 0;
+		padding: 0;
+		border: none;
+		line-height: inherit;
+		height: auto;
+		vertical-align: initial;
 	}
 }
 </style>
