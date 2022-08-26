085334afce2a82ddc5e8702e89f089daeea47728
Andreas Müller <mail@devmount.de>
2022-08-26 05:56:42 +0200

    🔨 migrate infosongdata  component

--- src/modals/InfoSongData.vue
+++ src/modals/InfoSongData.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="modal modal-md" :class="{ active: active }">
-		<a href="#" class="modal-overlay" aria-label="Close" @click.prevent="$emit('closed')"></a>
+		<a href="#" class="modal-overlay" aria-label="Close" @click.prevent="emit('closed')"></a>
 		<div class="modal-container">
 			<div class="modal-header">
-				<a href="#" class="btn btn-clear float-right" aria-label="Close" @click.prevent="$emit('closed')"></a>
-				<div class="modal-title h5">{{ $t('modal.songInfo') }}</div>
+				<a href="#" class="btn btn-clear float-right" aria-label="Close" @click.prevent="emit('closed')"></a>
+				<div class="modal-title h5">{{ t('modal.songInfo') }}</div>
 			</div>
 			<div class="modal-body">
 				<div v-if="song.note">
-					<div class="text-bold">{{ $t('field.note') }}</div>
+					<div class="text-bold">{{ t('field.note') }}</div>
 					<p v-html="song.note"></p>
 				</div>
 			</div>
@@ -17,14 +17,16 @@
 	</div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 
-export default defineComponent({
-	name: 'info-song-data',
-	props: {
-		active: Boolean,
-		song: Object
-	},
+// inherited properties
+const props = defineProps({
+	active: Boolean, // state of modal display, true to show modal
+	song: Object     // song to show information for
 });
+
+// emits
+const emit = defineEmits(['closed']);
 </script>
