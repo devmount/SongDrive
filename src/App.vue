fd40e726eb0f49e757d7ebcb4090abc4080ea9db
Andreas Müller <mail@devmount.de>
2022-12-26 15:55:35 +0100

    💚 style menu items

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,14 @@
 
 			<!-- off-screen sidebar -->
 			<div class="max-w-2xs px-2 py-8 bg-blade-200 dark:bg-blade-900" :class="{ active: open }">
-				<router-link to="/" class="flex flex-col w-max mx-auto mb-6">
+				<router-link to="/" class="flex flex-col w-max mx-auto mb-6 no-active">
 					<logo :featured="false" :show-version="true" />
 				</router-link>
 				<div class="flex flex-col">
 					<divider-horizontal :label="t('divider.start')" />
-					<li class="menu-item">
-						<router-link to="/" class="py-2" @click="open = false">
-							<ion-icon :icon="appsSharp" class="mr-2"></ion-icon> {{ t('page.dashboard') }}
-						</router-link>
-					</li>
+					<router-link to="/" class="px-3 py-2 flex items-center gap-3 hover:bg-blade-750" @click="open = false">
+						<ion-icon :icon="appsSharp" class="w-5 h-5" /> <span class="mb-0.5 uppercase">{{ t('page.dashboard') }}</span>
+					</router-link>
 					<li class="menu-item">
 						<router-link to="/songs" class="py-2" @click="open = false">
 							<ion-icon :icon="musicalNotesSharp" class="mr-2"></ion-icon> {{ t('page.songs') }}
