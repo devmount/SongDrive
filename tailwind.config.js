7a561e3ffa2f77784ec9140b715e020d3f24d589
Andreas Müller <mail@devmount.de>
2022-12-26 15:23:00 +0100

    ❌ remove Open Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
     fontFamily: {
-      sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
       mono: ['"Fira Mono"', ...defaultTheme.fontFamily.mono],
       fira: ['"Fira Sans"', ...defaultTheme.fontFamily.sans],
     },
