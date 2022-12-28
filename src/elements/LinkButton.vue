242e5368c5de82576cb9c948e9910b0a7d51fd7f
Andreas Müller <mail@devmount.de>
2022-12-28 21:21:53 +0100

    🔨 fix button display

--- src/elements/LinkButton.vue
+++ src/elements/LinkButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="
-    text-spring-600 flex justify-center items-center gap-2
+    text-spring-600 inline-flex justify-center items-center gap-2
     hover:text-black dark:hover:text-white
   ">
     <slot></slot>
