7a249e72de1ce84cba2db1eb0818f96635d6ad30
Andreas Müller <mail@devmount.de>
2023-01-25 07:45:37 +0100

    💚 convert icons and markup case in user unverified partial

--- src/partials/UserUnconfirmed.vue
+++ src/partials/UserUnconfirmed.vue
@@ -4,14 +4,14 @@
 		<logo :featured="true" :show-version="false" />
 		<!-- unconfirmed message -->
 		<panel class="max-w-sm w-full flex flex-col gap-6">
-			<div class="text-2xl text-center">{{ $t('text.waitingForApproval') }}</div>
-			<p>{{ $t('text.notApprovedYet') }}</p>
+			<div class="text-2xl text-center">{{ t('text.waitingForApproval') }}</div>
+			<p>{{ t('text.notApprovedYet') }}</p>
 			<p
 				v-if="ready.config && config.contact.email"
-				v-html="$t('text.unconfirmedMistake', [config.contact.email])"
+				v-html="t('text.unconfirmedMistake', [config.contact.email])"
 			></p>
-			<secondary-button @click="$emit('signOut')">
-				{{ $t('button.signOut') }}
+			<secondary-button @click="emit('signOut')">
+				{{ t('button.signOut') }}
 				<logout-icon class="w-6 h-6 stroke-1.5" />
 			</secondary-button>
 		</panel>
@@ -19,12 +19,19 @@
 </template>
 
 <script setup>
+import { useI18n } from "vue-i18n";
 import Logo from '@/partials/Logo';
 import Panel from '@/elements/Panel';
 import SecondaryButton from '@/elements/SecondaryButton';
 
 // icons
 import { LogoutIcon } from "vue-tabler-icons";
+
+// component constants
+const { t } = useI18n();
+
+// emits
+const emit = defineEmits(['signOut']);
 
 // component properties
 const props = defineProps({
--- src/partials/UserUnverified.vue
+++ src/partials/UserUnverified.vue
@@ -4,27 +4,27 @@
 		<logo :featured="true" :show-version="false" />
 		<!-- unverified message -->
 		<panel class="max-w-sm w-full flex flex-col gap-6">
-			<div class="text-2xl text-center">{{ $t('text.waitingForVerification') }}</div>
-			<p>{{ $t('text.notVerifiedYet') }}</p>
+			<div class="text-2xl text-center">{{ t('text.waitingForVerification') }}</div>
+			<p>{{ t('text.notVerifiedYet') }}</p>
 			<p
 				v-if="ready.config && config.contact.email"
-				v-html="$t('text.unverifiedHelp', [config.contact.email])"
+				v-html="t('text.unverifiedHelp', [config.contact.email])"
 			></p>
 			<secondary-button v-if="!resend" @click="resendVerification()">
-				{{ $t('tooltip.resendVerification') }}
-				<ion-icon :icon="sendOutline" class="w-6 h-6" />
+				{{ t('tooltip.resendVerification') }}
+				<send-icon class="w-6 h-6 stroke-1.5" />
 			</secondary-button>
 			<p v-else>
-				{{ $t('toast.verficationSentText') }}
+				{{ t('toast.verficationSentText') }}
 			</p>
 			<div class="flex gap-4">
-				<secondary-button @click="$router.go()">
-					{{ $t('button.recheck') }}
-					<ion-icon :icon="refreshOutline" class="w-6 h-6" />
+				<secondary-button @click="router.go()">
+					{{ t('button.recheck') }}
+					<refresh-icon class="w-6 h-6 stroke-1.5" />
 				</secondary-button>
-				<secondary-button @click="$emit('signOut')">
-					{{ $t('button.signOut') }}
-					<ion-icon :icon="logOutOutline" class="w-6 h-6" />
+				<secondary-button @click="emit('signOut')">
+					{{ t('button.signOut') }}
+					<logout-icon class="w-6 h-6 stroke-1.5" />
 				</secondary-button>
 			</div>
 		</panel>
@@ -32,22 +32,33 @@
 </template>
 
 <script setup>
+import { useI18n } from "vue-i18n";
+import { useRouter } from 'vue-router'
 import Logo from '@/partials/Logo';
 import Panel from '@/elements/Panel';
 import SecondaryButton from '@/elements/SecondaryButton';
 import { ref } from 'vue';
-import { sendOutline, refreshOutline, logOutOutline } from 'ionicons/icons';
 
+// icons
+import { SendIcon, RefreshIcon, LogoutIcon } from "vue-tabler-icons";
+
+// component constants
+const { t } = useI18n();
+const router = useRouter();
+
+// component properties
 const props = defineProps({
   ready: Object,
   config: Object,
 });
-const emit = defineEmits();
 
-// reactive data
+// emits
+const emit = defineEmits(['resendEmailVerification', 'signOut']);
+
+// prevent directly sending multiple emails
 const resend = ref(false);
 
-// methods
+// trigger sending verification email
 const resendVerification = () => {
 	resend.value = true;
 	emit('resendEmailVerification');
