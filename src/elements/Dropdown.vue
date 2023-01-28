f637ffaa77bbfa696a22119c1562c8c82209327e
Andreas Müller <mail@devmount.de>
2023-01-28 07:44:45 +0100

    🔨 fix dropdown item event handling

--- src/elements/Dropdown.vue
+++ src/elements/Dropdown.vue
@@ -1,9 +1,11 @@
 <template>
-	<slot name="trigger">
-		<secondary-button @click="open = !open" @focusout="open = false">
-			<dots-icon class="w-5 h-5 stroke-1.5" />
-		</secondary-button>
-	</slot>
+	<div @click="open = !open" @focusout="open = false" class="h-full">
+		<slot name="trigger">
+			<secondary-button class="h-full">
+				<dots-icon class="w-5 h-5 stroke-1.5" />
+			</secondary-button>
+		</slot>
+	</div>
 	<transition-scale>
 		<div v-if="open" class="relative">
 			<div class="absolute top-0 right-0 p-1 bg-blade-200 dark:bg-blade-900 z-10 w-max rounded-sm shadow-lg">
