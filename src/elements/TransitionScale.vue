716345c61cb28b34860b4b782da9f2ff3ebcc49a
Andreas Müller <mail@devmount.de>
2023-04-05 20:03:16 +0200

    🔨 fix dropdown transition z index

--- src/elements/TransitionScale.vue
+++ src/elements/TransitionScale.vue
@@ -2,10 +2,10 @@
 	<transition
 		enter-from-class="opacity-0 scale-90"
 		enter-active-class="transition-all ease-out"
-		enter-to-class="opacity-100 scale-100"
-		leave-from-class="opacity-100 scale-100"
+		enter-to-class="opacity-100 scale-100 z-50"
+		leave-from-class="opacity-100 scale-100 z-50"
 		leave-active-class="transition-all ease-in delay-150"
-		leave-to-class="opacity-0 scale-90"
+		leave-to-class="opacity-0 scale-90 z-50"
 	>
 		<slot></slot>
 	</transition>
