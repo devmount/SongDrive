e4c63f7fa2c01b72d160c797296f519a76bd66c2
Andreas Müller <mail@devmount.de>
2023-04-26 06:11:14 +0200

    💚 improve info zone appearance

--- src/elements/ZoneInfo.vue
+++ src/elements/ZoneInfo.vue
@@ -1,25 +1,25 @@
 <template>
+<div class="
+  relative rounded-sm border-blade-400 border-2 flex flex-col gap-4 p-4
+  dark:border-blade-800
+">
+  <!-- label -->
   <div class="
-    relative sm:min-w-[40vw] rounded-sm border-blade-400 border-2 flex flex-col gap-4 p-4
-    dark:border-blade-800
-  ">
-    <!-- label -->
-    <div class="
-      sm:absolute sm:-top-3 sm:left-2.5 uppercase tracking-widest text-sm sm:px-1.5
-      sm:bg-blade-100 sm:dark:bg-blade-850 text-blade-500
-    ">{{ label }}</div>
-    <!-- close button -->
-    <div
-      v-if="closable"
-      class="bg-blade-100 dark:bg-blade-850 text-blade-500 cursor-pointer absolute -top-3 -right-2"
-      @click="emit('close')"
-      :title="t('tooltip.hideThisSection')"
-    >
-      <icon-x class="w-5 h-5" />
-    </div>
-    <!-- content -->
-    <slot></slot>
+    -mt-7 px-1.5 -ml-1.5 inline-block uppercase max-w-max tracking-widest text-sm
+    bg-blade-100 dark:bg-blade-850 text-blade-500
+  ">{{ label }}</div>
+  <!-- close button -->
+  <div
+    v-if="closable"
+    class="bg-blade-100 dark:bg-blade-850 text-blade-500 cursor-pointer absolute -top-3 -right-2"
+    @click="emit('close')"
+    :title="t('tooltip.hideThisSection')"
+  >
+    <icon-x class="w-5 h-5" />
   </div>
+  <!-- content -->
+  <slot></slot>
+</div>
 </template>
 
 <script setup>
