a28e42036ae0e531f63a4333172ad8e84b44e62b
Andreas Müller <mail@devmount.de>
2024-01-14 00:33:01 +0100

    🔨 handle missing contact address

--- src/partials/UserUnconfirmed.vue
+++ src/partials/UserUnconfirmed.vue
@@ -6,11 +6,11 @@
 		<panel class="max-w-sm w-full flex flex-col gap-6">
 			<div class="text-2xl text-center">{{ t('text.waitingForApproval') }}</div>
 			<div>{{ t('text.notApprovedYet') }}</div>
-			<div v-if="ready.config && config.contact.email">
+			<div v-if="hasContact">
 				{{ t('text.unconfirmedMistake') }}
 			</div>
 			<div class="flex gap-2 w-full">
-				<secondary-button @click="mailto(config.contact.email)" class="grow">
+				<secondary-button v-if="hasContact" @click="mailto(config.contact.email)" class="grow">
 					{{ t('button.contactUs') }}
 					<icon-send class="w-6 h-6 stroke-1.5" />
 				</secondary-button>
@@ -24,6 +24,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { mailto } from '@/utils.js';
 import { useI18n } from 'vue-i18n';
 import Logo from '@/partials/Logo.vue';
@@ -44,4 +45,7 @@ const props = defineProps({
   ready: Object,
   config: Object,
 });
+
+// check if contact email address is configured and ready
+const hasContact = computed(() => props.ready.config && props.config?.contact?.email);
 </script>
