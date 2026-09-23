7baf8f713894a64a2ef123e744b1e0f19917d558
Andreas Müller <mail@devmount.de>
2026-09-23 23:09:03 +0200

    ➕️ Add export txt button

--- frontend/src/views/SetlistShow.vue
+++ frontend/src/views/SetlistShow.vue
@@ -137,6 +137,13 @@
 								</button>
 								<button
 									class="px-3 py-2 w-full flex items-center gap-3 hover:bg-blade-100 dark:hover:bg-blade-750"
+									@click="exportTxt()"
+								>
+									<icon-file-pencil class="w-5 h-5 stroke-1.5" />
+									{{ t('button.exportSetlistSheetsTxt') }}
+								</button>
+								<button
+									class="px-3 py-2 w-full flex items-center gap-3 hover:bg-blade-100 dark:hover:bg-blade-750"
 									@click="exportOsz()"
 								>
 									<icon-file class="w-5 h-5 stroke-1.5" />
@@ -222,6 +229,13 @@
 							>
 								<icon-files class="w-5 h-5 stroke-1.5" />
 								{{ t('button.exportSetlistSheets') }}
+							</button>
+							<button
+								class="px-3 py-2 w-full flex items-center gap-3 hover:bg-blade-100 dark:hover:bg-blade-750 sm:hidden"
+								@click="exportTxt()"
+							>
+								<icon-file-pencil class="w-5 h-5 stroke-1.5" />
+								{{ t('button.exportSetlistSheetsTxt') }}
 							</button>
 						</drop-down>
 					</div>
@@ -558,6 +572,7 @@ import {
 	IconEye,
 	IconFiles,
 	IconFile,
+	IconFilePencil,
 	IconFileText,
 	IconLock,
 	IconMarkdown,
